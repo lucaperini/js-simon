4764ba147be362f74e7482214941ee43db1dd59d
lucaperini <luca.perini@gmail.com>
2022-03-09 10:23:35 +0100

    correzione

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ setTimeout(maxTime, 30000);
 let randomNumbers = [];
 
 for (let i=1; i <=5; i++) {
-    let number = Math.floor(Math.random() * 5 +1 );
+    let number = Math.floor(Math.random() * 100 +1 );
     randomNumbers.push(number);
     document.querySelector('#numbers').innerHTML= randomNumbers.join(" ");
 }
@@ -33,4 +33,17 @@ function maxPrompt(){
     document.querySelector('#message').innerHTML= "Hai indovinato" + " " + checkNumbers.length + " " + "numeri." 
     + `<br>` + "Numeri generati:" + " " + randomNumbers.join("-") + `<br>` + "I tuoi numeri:" + " " + checkNumbers.join("-");
     } 
+}
+
+
+function generateUniqueRandomNumber(numsBlacklist, min, max){
+    let check = false;
+    let randomInt;
+    while (!check){
+    randomInt=Math.floor(Math.random()*((max + 1) - min) + min);
+    if (!numsBlacklist.includes(randomInt)){
+    check = true;
+    return randomInt;
+    }
+}
 }
